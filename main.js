19962cbe53a255c6d20a4c573b95841d3351b8b6
Nico <72673830+NicolasHrt@users.noreply.github.com>
2022-11-15 12:17:06 +0000

    remove 8k texture

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 import * as THREE from "three";
-import { Plane } from "three";
 
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { ceilPowerOfTwo } from "three/src/math/mathutils";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+// import { ceilPowerOfTwo } from "three/src/math/mathutils";
 
 // Language: javascript
 // Path: vite-project\main.js
 const Planets = [];
 const planetCanvas = document.getElementsByClassName("planet-image");
-console.log(planetCanvas);
 
 const slides = document.getElementsByClassName("planet-card");
 const totalSlides = slides.length;
@@ -28,7 +26,7 @@ camera.position.setZ(30);
 
 const mercuryGeometry = new THREE.SphereGeometry(15);
 const mercuryTexture = new THREE.TextureLoader().load(
-  "./textures/8k_mercury.jpg"
+  "./textures/2k_mercury.jpg"
 );
 const mercuryMaterial = new THREE.MeshBasicMaterial({ map: mercuryTexture });
 const mercury = new THREE.Mesh(mercuryGeometry, mercuryMaterial);
@@ -36,7 +34,7 @@ Planets.push(mercury);
 
 const venusGeometry = new THREE.SphereGeometry(15);
 const venusTexture = new THREE.TextureLoader().load(
-  "./textures/8k_venus_surface.jpg"
+  "./textures/2k_venus_surface.jpg"
 );
 const venusMaterial = new THREE.MeshBasicMaterial({ map: venusTexture });
 const venus = new THREE.Mesh(venusGeometry, venusMaterial);
@@ -44,7 +42,7 @@ Planets.push(venus);
 
 const earthGeometry = new THREE.SphereGeometry(15);
 const earthTexture = new THREE.TextureLoader().load(
-  "./textures/8k_earth_daymap.jpg"
+  "./textures/2k_earth_daymap.jpg"
 );
 const earthMaterial = new THREE.MeshBasicMaterial({ map: earthTexture });
 const earth = new THREE.Mesh(earthGeometry, earthMaterial);
@@ -52,7 +50,7 @@ Planets.push(earth);
 
 const marsGeometry = new THREE.SphereGeometry(15);
 const marsTexture = new THREE.TextureLoader().load(
-  "./textures/8k_mars.jpg"
+  "./textures/2k_mars.jpg"
 );
 const marsMaterial = new THREE.MeshBasicMaterial({ map: marsTexture });
 const mars = new THREE.Mesh(marsGeometry, marsMaterial);
@@ -60,7 +58,7 @@ Planets.push(mars);
 
 const jupiterGeometry = new THREE.SphereGeometry(15);
 const jupiterTexture = new THREE.TextureLoader().load(
-  "./textures/8k_jupiter.jpg"
+  "./textures/2k_jupiter.jpg"
 );
 const jupiterMaterial = new THREE.MeshBasicMaterial({ map: jupiterTexture });
 const jupiter = new THREE.Mesh(jupiterGeometry, jupiterMaterial);
@@ -68,7 +66,7 @@ Planets.push(jupiter);
 
 const saturnGeometry = new THREE.SphereGeometry(15);
 const saturnTexture = new THREE.TextureLoader().load(
-  "./textures/8k_saturn.jpg"
+  "./textures/2k_saturn.jpg"
 );
 const saturnMaterial = new THREE.MeshBasicMaterial({ map: saturnTexture });
 const saturn = new THREE.Mesh(saturnGeometry, saturnMaterial);
@@ -112,7 +110,7 @@ scene.add( stars );
 const ReloadScene = () => {
 
   var container = planetCanvas[slidePosition];
-  console.log(container);
+
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(container.offsetWidth, container.offsetHeight);
   scene.add(Planets[slidePosition]);
@@ -167,7 +165,6 @@ const movetoPreviousSlide = () => {
 };
 
 const updateSlidePosition = () => {
-  console.log(slidePosition);
   for (let slide of slides) {
     slide.classList.add("planet-card--hidden");
     slide.classList.remove("planet-card--visible");
